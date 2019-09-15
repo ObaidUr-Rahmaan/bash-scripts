4f851fcdb3bb66b1c60f93e09cfd1c9da775d27c
Obaid Ur-Rahmaan <oxu711@cs.bham.ac.uk>
2019-09-15 13:01:00 +0100

    Update and bug fix: See README file

--- pomo.sh
+++ pomo.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
-counter=0
-limit=$1
-summary=$2
-startmessage=$3
-endmessage=$4
-notify-send -u critical -i appointment -t 600 "$summary" "$startmessage"
-# notifu /m "Timer Started. Get to Work!" /d 5000
+work_counter=0
+break_counter=0
+work_limit=$1
+break_limit=$2
+summary=$3
+work_message=$4
+break_message=$5
+end_message=$6
+notify-send -u critical -i appointment -t 600 "$summary" "$work_message"
 echo
-while [ $counter != $limit ]; do
-   echo "$counter minutes so far...";
-   sleep 60
-   let "counter = $counter + 1"
+while [ $work_counter != $work_limit ]; do
+   echo "$work_counter minutes so far...";
+   sleep 1
+   let "work_counter = $work_counter + 1"
 done
-if [ $counter = $limit ]; then
+if [ $work_counter = $work_limit ]; then
    echo
-   # notify-send -u critical -i appointment "$summary" "$endmessage"
-   notifu /m "Timer ended. Take a break" /d 5000
-   echo -e '\a' &gt;&amp;2
-   exit 0
+   notify-send -u critical -i appointment -t 600 "$summary" "$break_message"
+   echo
+   while [ $break_counter != $break_limit ]; do
+      echo "$break_counter minutes so far...";
+      sleep 1
+      let "break_counter = $break_counter + 1"
+   done
+   if [ $break_counter = $break_limit ]; then
+      echo
+      notify-send -u critical -i appointment "$summary" "$end_message"
+      exit 0
+   fi
 fi
